e400e145bbb48d6e113a9f80a85a31d1a0dc7f08
Prasad Cholakkottil <prasadck@maveric-systems.com>
2022-06-22 15:27:39 +0530

    template resolver and path corrections

--- src/main/java/kube/demo/spring/data/mongodb/KubeDemoMongodbApplication.java
+++ src/main/java/kube/demo/spring/data/mongodb/KubeDemoMongodbApplication.java
@@ -2,12 +2,28 @@ package kube.demo.spring.data.mongodb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 
 @SpringBootApplication
 public class KubeDemoMongodbApplication {
 
+	public static ApplicationContext applicationContext;
+	
 	public static void main(String[] args) {
-		SpringApplication.run(KubeDemoMongodbApplication.class, args);
+		applicationContext = SpringApplication.run(KubeDemoMongodbApplication.class, args);
 	}
 
+	@Bean
+	public SpringResourceTemplateResolver templateResolver() {
+	    SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+	    templateResolver.setApplicationContext(applicationContext);
+	    templateResolver.setCharacterEncoding("UTF-8");
+	    templateResolver.setPrefix("classpath:/templates/");
+	    templateResolver.setSuffix(".html");
+	    templateResolver.setCacheable(false);
+
+	    return templateResolver;
+	}
 }
