c96ffeb778b290852345b0da563c31c68438ab53
Prasad Cholakkottil <prasadck@maveric-systems.com>
2022-07-08 14:31:04 +0530

    add infy, remove table

--- src/main/java/kube/demo/spring/data/mongodb/controller/LogController.java
+++ src/main/java/kube/demo/spring/data/mongodb/controller/LogController.java
@@ -1,8 +1,11 @@
 package kube.demo.spring.data.mongodb.controller;
 
-import java.time.Instant;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoOperations;
@@ -33,9 +36,9 @@ public class LogController {
 	private String db = "local";
 	private String user = "adminuser";
 	private String pass = "password123";
-	private String collection = "nse_data";
-	//private String host = "a22437652ec75464b9aec75355b895fe-699670675.us-east-2.elb.amazonaws.com";
-	private String host = "localhost";
+	private String collection = "yf-stock-minutes";
+	private String host = "a22437652ec75464b9aec75355b895fe-699670675.us-east-2.elb.amazonaws.com";
+	//private String host = "localhost";
 	private String port = "27017";
 	
 	
@@ -81,9 +84,9 @@ public class LogController {
 		
 		try {
 			MongoOperations mongoOps = new MongoTemplate(new SimpleMongoClientDatabaseFactory(getConnString()));
-			Sort s = Sort.by(Sort.Direction.DESC, "currentTime");
+			Sort s = Sort.by(Sort.Direction.DESC, "Recorded_time");
 			Query q = new Query().with(s).limit(Integer.parseInt(count));
-			stockData = mongoOps.find(q, StockData.class, "nse_data");
+			stockData = mongoOps.find(q, StockData.class, "yf_nse_data");
     
 			if (stockData.isEmpty()) {
 				model.addAttribute("datalist", "");
@@ -93,23 +96,38 @@ public class LogController {
 			return e.getMessage();
 		}
 		
-		List<Object> datalist = new ArrayList<>();
-		for (StockData s : stockData) {
-			List<Object> list = new ArrayList<>();
-			list.add(s.getCurrentTime());
-			if ("INFY.NS".equals(s.getStockName())) {
-				list.add(null);
-				list.add(s.getClose());
-			} else {
-				list.add(s.getClose());
-				list.add(null);
-			}
+		List<Object> infylist = new ArrayList<>();
+		List<Object> tcslist = new ArrayList<>();
+		Map<String, Double[]> data = new TreeMap<>();
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy M dd HH mm");
+		for (StockData stockDoc : stockData) {
+			String currentMinuteKey = sdf.format(stockDoc.getId().getDate());
+			Double[] list = data.get(currentMinuteKey);
+			if (list == null )
+				list = new Double[2];  
 			
-			datalist.add(list);
+			//list.add(s.getCurrentTime());
+			if ("INFY.NS".equals(stockDoc.getStockName())) {
+				list[1] = stockDoc.getClose();
+			} else if ("TCS.NS".equals(stockDoc.getStockName())) {
+				list[0] = stockDoc.getClose();
+			}
+			//System.out.println(list[0] + "," + list[1]);
+			data.put(currentMinuteKey, list);
 		}
 		
-		model.addAttribute("data", stockData);
-		model.addAttribute("datalist", datalist);
+		data.entrySet().forEach(entry -> {
+		    System.out.println(entry.getKey() + ": " + entry.getValue()[0] + ", " + entry.getValue()[1]);
+		    if (entry.getValue()[0] != null &&  entry.getValue()[1] != null) {
+		    	tcslist.add(Arrays.asList(entry.getKey(), entry.getValue()[0]));
+		    	infylist.add(Arrays.asList(entry.getKey(), entry.getValue()[1]));
+		    }
+		});
+		
+		//model.addAttribute("data", stockData);
+		model.addAttribute("infylist", infylist);
+		model.addAttribute("tcslist", tcslist);
 		return "stockdata";
 	}
 
